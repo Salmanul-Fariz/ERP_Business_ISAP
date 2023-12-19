cd90ca77c30358a9075997f1c99ddb73d9d8f17e
Salmanul Fariz N <salmanulfariz997@gmail.com>
2023-12-19 21:14:44 +0530

    Completed the section 1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component } from '@angular/core';
 import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
@@ -6,7 +6,7 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
-export class AppComponent {
+export class AppComponent implements AfterViewInit {
   customOptions: OwlOptions = {
     loop: true,
     mouseDrag: false,
@@ -70,4 +70,44 @@ export class AppComponent {
       src: 'https://isap.solutions/wp-content/uploads/2023/06/ipeye-150x150.png',
     },
   ];
+
+  // Subscribe to NgbCarousel slide event on initialization
+  ngAfterViewInit() {
+    if (typeof document !== 'undefined') {
+      this.zoomCarouselImage('0');
+
+      const myCarousel = document?.getElementById(
+        'carouselExampleCaptions'
+      ) as HTMLElement;
+
+      if (myCarousel)
+        myCarousel.addEventListener('slide.bs.carousel', (event: any) => {
+          this.zoomCarouselImageRemove();
+
+          const targetIndex = event.to;
+
+          setTimeout(() => {
+            this.zoomCarouselImage(targetIndex);
+          }, 900);
+        });
+    }
+  }
+
+  zoomCarouselImage(id: string) {
+    const image = document.getElementById(
+      `carousel_image_${id}`
+    ) as HTMLImageElement;
+
+    image.style.transform = 'scale(1.06)';
+  }
+
+  zoomCarouselImageRemove() {
+    for (let i = 0; i < 3; i++) {
+      const image = document.getElementById(
+        `carousel_image_${i}`
+      ) as HTMLImageElement;
+
+      image.style.transform = 'scale(1)';
+    }
+  }
 }
