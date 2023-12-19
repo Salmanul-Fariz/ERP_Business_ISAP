1ec5d1a564c93a4de70be4cac386cea700a09cbd
Salmanul Fariz N <salmanulfariz997@gmail.com>
2023-12-19 21:55:26 +0530

    Completed the service carousel section

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,65 +9,103 @@ import { OwlOptions } from 'ngx-owl-carousel-o';
 export class AppComponent implements AfterViewInit {
   customOptions: OwlOptions = {
     loop: true,
-    mouseDrag: false,
-    touchDrag: false,
+    mouseDrag: true,
+    touchDrag: true,
     pullDrag: false,
-    dots: false,
+    dots: true,
     navSpeed: 700,
-    navText: ['', ''],
     responsive: {
       0: {
-        items: 1,
-      },
-      400: {
         items: 2,
       },
+      400: {
+        items: 4,
+      },
       740: {
-        items: 3,
+        items: 5,
       },
       940: {
-        items: 8,
+        items: 6,
       },
     },
-    nav: true,
   };
 
   slidesStore = [
     {
       id: '1',
-      src: 'https://isap.solutions/wp-content/uploads/2023/06/ipeye-150x150.png',
+      src: 'assets/images/services/job-150x150.png',
     },
     {
-      id: '1',
-      src: 'https://isap.solutions/wp-content/uploads/2023/06/ipeye-150x150.png',
+      id: '2',
+      src: 'assets/images/services/solar-150x150.png',
     },
     {
-      id: '1',
-      src: 'https://isap.solutions/wp-content/uploads/2023/06/ipeye-150x150.png',
+      id: '3',
+      src: 'assets/images/services/nft-150x150.png',
     },
     {
-      id: '1',
-      src: 'https://isap.solutions/wp-content/uploads/2023/06/ipeye-150x150.png',
+      id: '4',
+      src: 'assets/images/services/network-150x150.png',
     },
     {
-      id: '1',
-      src: 'https://isap.solutions/wp-content/uploads/2023/06/ipeye-150x150.png',
+      id: '5',
+      src: 'assets/images/services/metaverse-150x150.png',
     },
     {
-      id: '1',
-      src: 'https://isap.solutions/wp-content/uploads/2023/06/ipeye-150x150.png',
+      id: '6',
+      src: 'assets/images/services/legal-150x150.png',
     },
     {
-      id: '1',
-      src: 'https://isap.solutions/wp-content/uploads/2023/06/ipeye-150x150.png',
+      id: '7',
+      src: 'assets/images/services/ISAP-School-logo-150x150.png',
     },
     {
-      id: '1',
-      src: 'https://isap.solutions/wp-content/uploads/2023/06/ipeye-150x150.png',
+      id: '8',
+      src: 'assets/images/services/isap-phone-black-150x150.png',
     },
     {
-      id: '1',
-      src: 'https://isap.solutions/wp-content/uploads/2023/06/ipeye-150x150.png',
+      id: '9',
+      src: 'assets/images/services/isap-life-black-150x150.png',
+    },
+    {
+      id: '10',
+      src: 'assets/images/services/ISAP-HEALTH-B-150x150.png',
+    },
+    {
+      id: '11',
+      src: 'assets/images/services/ISAP-Green-150x150.png',
+    },
+    {
+      id: '12',
+      src: 'assets/images/services/isap-clinic-150x150.png',
+    },
+    {
+      id: '13',
+      src: 'assets/images/services/isap-CC-150x150.png',
+    },
+    {
+      id: '14',
+      src: 'assets/images/services/isap-black-150x150.png',
+    },
+    {
+      id: '15',
+      src: 'assets/images/services/ipeye-150x150.png',
+    },
+    {
+      id: '16',
+      src: 'assets/images/services/courses-150x150.png',
+    },
+    {
+      id: '17',
+      src: 'assets/images/services/b-digital-150x150.png',
+    },
+    {
+      id: '18',
+      src: 'assets/images/services/b-crypto-150x150.png',
+    },
+    {
+      id: '19',
+      src: 'assets/images/services/bakalat-150x150.png',
     },
   ];
 
